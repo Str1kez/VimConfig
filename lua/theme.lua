f2815fb921e3931c1b3ea55b8aece57f997d1b92
Str1kez <vazinov@yandex.ru>
2022-12-26 22:24:30 +0300

    📄 kanagawa theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -16,5 +16,19 @@ g.gruvbox_material_spell_foreground = 'colored'
 g.gruvbox_material_diagnostic_text_highlight = 1
 g.sonokai_better_performance = 1
 g.material_theme_style = 'darker'
-cmd'colorscheme gruvbox-material' 
+local my_colors = {
+    waveBlue1 = "#415D87",
+    waveBlue2 = "#4970AC",
+}
+require'kanagawa'.setup({
+    commentStyle = { italic = false },
+    functionStyle = {},
+    keywordStyle = { italic = false},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = false},
+    colors = my_colors}
+    )
+
+cmd'colorscheme kanagawa' 
 g.transparent_enabled = true
