1369afd97cc458eb134a5d0e56028c0f1f3b6fec
Str1kez <vazinov@yandex.ru>
2023-04-19 16:25:48 +0300

    📄 updated nvim-tree behavior

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,18 +1,16 @@
+local on_attach = require "plugins.nvim-tree-on-attach"
+
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
+    on_attach = on_attach,
     view = {
-        width = {
-          max = 40,
-        },
-        adaptive_size = true,
-        mappings = {
-        list = {
-            { key = "=", action = "cd" },
-        },
-        },
+      width = {
+        max = 40,
+      },
+      adaptive_size = true,
     },
     renderer = {
-        group_empty = true,
+      group_empty = true,
     },
     actions = {
       open_file = {
@@ -20,7 +18,7 @@ require("nvim-tree").setup({
       },
     },
     filters = {
-        dotfiles = true,
+      dotfiles = true,
     },
   }
 )
