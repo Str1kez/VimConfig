5d76f90f70dbc1f35eb7ed8b6c115ac365860fcb
Str1kez <vazinov@yandex.ru>
2023-05-25 02:08:29 +0300

    📄 fix: color on bg4 for sonokai, helps to detect paired bracket

--- lua/themes/sonokai.lua
+++ lua/themes/sonokai.lua
@@ -4,4 +4,4 @@ local g = vim.g
 g.sonokai_better_performance = 1
 g.sonokai_diagnostic_virtual_text = 'colored'
 g.sonokai_diagnostic_line_highlight = 1
-g.sonokai_colors_override = { bg0 = { '#22262e', '230' }, bg3 = { '#595f6f', '236' }, bg_dim={'#22262e', '230'} }
+g.sonokai_colors_override = { bg0 = { '#22262e', '230' }, bg3 = { '#595f6f', '236' }, bg_dim={'#22262e', '230'}, bg4={'#565b6b', '235'} }
