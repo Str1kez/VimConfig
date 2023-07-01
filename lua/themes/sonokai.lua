3f92e2628d306d4adda7a2d0f10d346a19ef33c3
Str1kez <vazinov@yandex.ru>
2023-07-01 22:08:22 +0300

    📄 changed: sonokai primary color

--- lua/themes/sonokai.lua
+++ lua/themes/sonokai.lua
@@ -4,4 +4,4 @@ local g = vim.g
 g.sonokai_better_performance = 1
 g.sonokai_diagnostic_virtual_text = 'colored'
 g.sonokai_diagnostic_line_highlight = 1
-g.sonokai_colors_override = { bg0 = { '#1b1e24', '230' }, bg3 = { '#595f6f', '236' }, bg_dim={'#1b1e24', '230'}, bg4={'#565b6b', '235'} }
+g.sonokai_colors_override = { bg0 = { '#282939', '230' }, bg3 = { '#595f6f', '236' }, bg_dim={'#282939', '230'}, bg4={'#565b6b', '235'} }
